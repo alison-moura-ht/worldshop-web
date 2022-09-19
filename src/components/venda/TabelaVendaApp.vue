fd76f66f5f33e0e2a089eb7db3ff48aeee668349
Alison Moura <alison.oghino@gmail.com>
2022-09-19 19:30:53 -0400

    Implementando formatação de data com Moment Timezone

--- src/components/venda/TabelaVendaApp.vue
+++ src/components/venda/TabelaVendaApp.vue
@@ -10,7 +10,7 @@
     <tbody>
       <tr v-for="item in vendas" :key="item._id">
         <td>{{ formataDecimal(item.valorTotal) }}</td>
-        <td>{{ item.data }}</td>
+        <td>{{ formataData(item.data) }}</td>
         <td>{{ item.cliente.nome }}</td>
         <td>{{ item.usuario.nome }}</td>
         <td>
@@ -25,6 +25,7 @@
 <script>
 import { mapState } from "pinia";
 import { useVendaStore } from "../../stores/vendaStore";
+import moment from "moment-timezone"
 
 export default {
   computed: {
@@ -38,6 +39,10 @@ export default {
         minimumFractionDigits: 2,
       });
     },
+    formataData(data) {
+      const dataMoment = moment(data)
+      return dataMoment.tz("America/Campo_Grande").format("DD/MM/YYYY")
+    }
   },
 };
 </script>
